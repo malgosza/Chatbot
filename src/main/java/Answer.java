1ff8ab40fd5e703f09e000bef5360a7a8d3c9e5d
Kamil <kamil.figon@gmail.com>
2020-07-01 11:00:47 +0200

    Added Question, Answer and Conversation class

---
+++ src/main/java/Answer.java
@@ -0,0 +1,11 @@
+public class Answer {
+    int idAnswer;
+    String answer;
+    Integer idQuestion;
+
+    public Answer(int idAnswer,String answer,Integer idQuestion){
+        this.idAnswer=idAnswer;
+        this.answer=answer;
+        this.idQuestion=idQuestion;
+    }
+}
--- src/main/java/Question.java
+++ src/main/java/Question.java
@@ -2,4 +2,10 @@ public class Question {
     int idQuestion;
     String question;
     Integer idAnswer;
+
+    public Question(int idQuestion, String question,Integer idAnswer){
+        this.idQuestion=idQuestion;
+        this.question=question;
+        this.idAnswer=idAnswer;
+    }
 }
